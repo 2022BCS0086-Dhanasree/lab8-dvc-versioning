Fix RMSE calculation

src/train.py
@@ -30,7 +30,11 @@ model.fit(X_train, y_train)
 pred = model.predict(X_test)
 
 # Calculate metrics
-rmse = mean_squared_error(y_test, pred, squared=False)
+import numpy as np
+
+pred = model.predict(X_test)
+
+rmse = np.sqrt(mean_squared_error(y_test, pred))
 r2 = r2_score(y_test, pred)
 
 metrics = {